9e5e974f262319aa7677e7682e4e47658d06c18c
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-05-16 15:16:55 +0300

    practice q6

--- app.js
+++ app.js
@@ -7,44 +7,47 @@ app.use(express.json());
 
 app.get("/todos", (req, res) => {
     todoModel.find({})
-  .then((result)=>{
-    res.json(result);
-  }).catch((err)=>{res.json(err)})
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => { res.json(err) })
 
 });
 
-app.get("/todos/complete",(req,res)=>{
-    todoModel.find({isCompleted:true})
-    .then((result)=>{
-      res.json(result);
-    }).catch((err)=>{res.json(err)})
+app.get("/todos/complete", (req, res) => {
+    todoModel.find({ isCompleted: true })
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => { res.json(err) })
 })
 
 app.post("/create/todo", (req, res) => {
-    const {task, description, deadline, isCompleted,priority}= req.body;
+    const { task, description, deadline, isCompleted, priority } = req.body;
 
-    const todos=new todoModel(
-        {task, description, deadline, isCompleted,priority}
+    const todos = new todoModel(
+        { task, description, deadline, isCompleted, priority }
     )
-    todos.save().then(result=>{ res.json(result)}).catch(err=>{res.send(err)})
+    todos.save().then(result => { res.json(result) }).catch(err => { res.send(err) })
 });
 
 
 
 app.put("/update/todo", (req, res) => {
-    //const {task, description, deadline, isCompleted,priority}= req.body;
- 
- console.log(req.body.task);
- const query = {task:"roaa"};
- todoModel.findOneAndUpdate(query,{task:req.body.task}
+    const query = { task: "roaamagaydah" };
+    todoModel.findOneAndUpdate(query, { task, description, deadline, isCompleted, priority } = req.body
     )
- .then(result=>{ res.json(result)}).catch(err=>{res.send(err)})
+        .then(result => { res.json(result) }).catch(err => { res.send(err) })
 });
 
 
-app.delete("/delete/todo", (req, res) => {});
+app.delete("/delete/todo", (req, res) => {
+    const query = { task: "sport_1" };
+    todoModel.deleteOne(query)
+        .then(result => { res.json("done") }).catch(err => { res.send(err) })
+
+});
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
+
